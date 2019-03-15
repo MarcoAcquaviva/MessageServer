12bb1ad739554da7d179543355af1a3c94877e67
Acquaviva Marco <acquavivamarco@hotmail.it>
2019-03-15 23:53:59 +0100

    Exend Packet.cs, Add Commands List, Add Basic Math Funtcion and relative Tests

--- ServerMessagingApp/ServerMessagingApp.Test/TestGameServer.cs
+++ ServerMessagingApp/ServerMessagingApp.Test/TestGameServer.cs
@@ -33,16 +33,16 @@ namespace ServerMessagingApp.Test
         [Test]
         public void TestRecivePacket()
         {
-            Packet packet = new Packet(0,"ciao");
-            transport.ClientEnqueue(packet,"test",0);
+            Packet packet = new Packet(0, "ciao");
+            transport.ClientEnqueue(packet, "test", 0);
             Assert.That(transport.ServerQueueCount, Is.EqualTo(1));
         }
 
         [Test]
         public void TestSendPacketToClient()
         {
-            Packet packet = new Packet(0,"ciao");
-            transport.ClientEnqueue(packet,"test",0);
+            Packet packet = new Packet(0, "ciao");
+            transport.ClientEnqueue(packet, "test", 0);
             server.SingleStep();
             Assert.That(transport.ClientQueueCount, Is.EqualTo(1));
         }
@@ -50,17 +50,17 @@ namespace ServerMessagingApp.Test
         [Test]
         public void TestClientRecivePacket()
         {
-            Packet packet = new Packet(0,"ciao");
+            Packet packet = new Packet(0, "ciao");
             transport.ClientEnqueue(packet, "test", 0);
             server.SingleStep();
             transport.ClientDequeue();
             Assert.That(transport.ClientQueueCount, Is.EqualTo(0));
         }
-        
+
         [Test]
         public void TestPacketHasNotSent()
         {
-            Packet packet = new Packet(0,"ciao");
+            Packet packet = new Packet(0, "ciao");
             transport.ClientEnqueue(packet, "test", 0);
             server.SingleStep();
             transport.ClientDequeue();
@@ -72,7 +72,7 @@ namespace ServerMessagingApp.Test
         [Test]
         public void TestDataRecived()
         {
-            Packet packet = new Packet(0,"ciao");
+            Packet packet = new Packet(0, "ciao");
             transport.ClientEnqueue(packet, "test", 0);
             FakeData data = transport.ServerDequeue();
             string text = Encoding.UTF8.GetString(data.data);
@@ -83,7 +83,7 @@ namespace ServerMessagingApp.Test
         [Test]
         public void TestDataSent()
         {
-            Packet packet = new Packet(0,"ciao");
+            Packet packet = new Packet(0, "ciao");
             transport.ClientEnqueue(packet, "test", 0);
             server.SingleStep();
             FakeData data = transport.ClientDequeue();
@@ -92,32 +92,29 @@ namespace ServerMessagingApp.Test
             Assert.That(text, Is.EqualTo("oaic"));
         }
 
-        
         [Test]
-        public void TestServerAddPacketLength()
+        public void TestServerMathsOperationPacketLength()
         {
-            Packet packet = new Packet(1,'i',1,1);
+            Packet packet = new Packet(1, 'i', 1, 1);
             transport.ClientEnqueue(packet, "test", 0);
             FakeData data = transport.ServerDequeue();
             Assert.That(data.data.Length, Is.EqualTo(10));
         }
 
-                
         [Test]
-        public void TestClientAddPacketLength()
+        public void TestClientMathsOperationPacketLength()
         {
-            Packet packet = new Packet(1,'i',1,1);
+            Packet packet = new Packet(1, 'i', 1, 1);
             transport.ClientEnqueue(packet, "test", 0);
             server.SingleStep();
-            FakeData data = transport.ClientDequeue();     
+            FakeData data = transport.ClientDequeue();
             Assert.That(data.data.Length, Is.EqualTo(6));
         }
 
-                        
         [Test]
-        public void TestClientReciveCorrectSum()
+        public void TestClientReciveCorrectSumInteger()
         {
-            Packet packet = new Packet(1,'i',1,1);
+            Packet packet = new Packet(1, 'i', 1, 1);
             transport.ClientEnqueue(packet, "test", 0);
             server.SingleStep();
             FakeData data = transport.ClientDequeue();
@@ -125,6 +122,125 @@ namespace ServerMessagingApp.Test
             Assert.That(sum, Is.EqualTo(2));
         }
 
+        [Test]
+        public void TestClientReciveCorrectSumFloat()
+        {
+            Packet packet = new Packet(1, 'f', 1.0f, 1.0f);
+            transport.ClientEnqueue(packet, "test", 0);
+            server.SingleStep();
+            FakeData data = transport.ClientDequeue();
+            float sum = BitConverter.ToSingle(data.data, 2);
+            Assert.That(sum, Is.EqualTo(2.0f));
+        }
 
+
+        [Test]
+        public void TestClientReciveErrorPacketOnWrongType()
+        {
+            Packet packet = new Packet(1, 'd', 1.0f, 1.0f);
+            transport.ClientEnqueue(packet, "test", 0);
+            server.SingleStep();
+
+            FakeData data = transport.ClientDequeue();
+            string text = Encoding.UTF8.GetString(data.data);
+            server.RemoveIndexCharFromString(ref text, 0);
+
+            Assert.That(data.data.Length, Is.Not.EqualTo(6));
+            Assert.That(text, Is.EqualTo("Type must be both integer or float"));
+        }
+
+        [Test]
+        public void TestClientReciveCorrectSubstractIntegerNumbers()
+        {
+            Packet packet = new Packet(2, 'i', 1, 1);
+            transport.ClientEnqueue(packet, "test", 0);
+            server.SingleStep();
+            FakeData data = transport.ClientDequeue();
+            int sub = BitConverter.ToInt32(data.data, 2);
+            Assert.That(sub, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestClientReciveCorrectSubstractFloatNumbers()
+        {
+            Packet packet = new Packet(2, 'f', 1.0f, 1.0f);
+            transport.ClientEnqueue(packet, "test", 0);
+            server.SingleStep();
+            FakeData data = transport.ClientDequeue();
+            float sub = BitConverter.ToSingle(data.data, 2);
+            Assert.That(sub, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestClientReciveCorrectMultiplyIntegerNumbers()
+        {
+            Packet packet = new Packet(3, 'i', 1, 1);
+            transport.ClientEnqueue(packet, "test", 0);
+            server.SingleStep();
+            FakeData data = transport.ClientDequeue();
+            int mul = BitConverter.ToInt32(data.data, 2);
+            Assert.That(mul, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TestClientReciveCorrectMultiplyFloatNumbers()
+        {
+            Packet packet = new Packet(3, 'f', 1.0f, 1.0f);
+            transport.ClientEnqueue(packet, "test", 0);
+            server.SingleStep();
+            FakeData data = transport.ClientDequeue();
+            float mul = BitConverter.ToSingle(data.data, 2);
+            Assert.That(mul, Is.EqualTo(1.0f));
+        }
+
+        [Test]
+        public void TestClientReciveCorrectDivisionIntegerNumbers()
+        {
+            Packet packet = new Packet(4, 'i', 4, 2);
+            transport.ClientEnqueue(packet, "test", 0);
+            server.SingleStep();
+            FakeData data = transport.ClientDequeue();
+            int div = BitConverter.ToInt32(data.data, 2);
+            Assert.That(div, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void TestClientReciveCorrectDivisionFloatNumbers()
+        {
+            Packet packet = new Packet(4, 'f', 4.0f, 2.0f);
+            transport.ClientEnqueue(packet, "test", 0);
+            server.SingleStep();
+            FakeData data = transport.ClientDequeue();
+            float div = BitConverter.ToSingle(data.data, 2);
+            Assert.That(div, Is.EqualTo(2.0f));
+        }
+
+        [Test]
+        public void TestClientReciveErrorPacketOnDivisionZeroIntegerNumbers()
+        {
+            Packet packet = new Packet(4, 'i', 4, 0);
+            transport.ClientEnqueue(packet, "test", 0);
+            server.SingleStep();
+
+            FakeData data = transport.ClientDequeue();
+            string text = Encoding.UTF8.GetString(data.data);
+            server.RemoveIndexCharFromString(ref text, 0);
+
+            Assert.That(text, Is.EqualTo("The second number cannot be 0"));
+        }
+
+        [Test]
+        public void TestClientReciveErrorPacketOnDivisionZeroFloatNumbers()
+        {
+            Packet packet = new Packet(4, 'f', 4.0f, 0);
+            transport.ClientEnqueue(packet, "test", 0);
+            server.SingleStep();
+
+            FakeData data = transport.ClientDequeue();
+            string text = Encoding.UTF8.GetString(data.data);
+            server.RemoveIndexCharFromString(ref text, 0);
+
+            Assert.That(text, Is.EqualTo("The second number cannot be 0"));
+        }
     }
 }
--- ServerMessagingApp/ServerMessagingApp/GameServer.cs
+++ ServerMessagingApp/ServerMessagingApp/GameServer.cs
@@ -21,12 +21,12 @@ namespace ServerMessagingApp
         #region Server Command Methods
         public void ReverseString(byte[] data, EndPoint sender)
         {
-            if (data != null && data.Length >1)
+            if (data != null && data.Length > 1)
             {
                 Console.WriteLine("Recive message from {0} with {1} ", sender, data[0]);
                 string text = Encoding.UTF8.GetString(data);
                 RemoveIndexCharFromString(ref text, 0);
-                Packet packet = new Packet(0,Reverse(text));
+                Packet packet = new Packet(0, Reverse(text));
                 SendToClient(packet, sender);
             }
         }
@@ -38,14 +38,26 @@ namespace ServerMessagingApp
                 byte commandID = data[0];
                 Packet packet = new Packet();
 
-                char type = Encoding.UTF8.GetChars(data,1,1)[0];
+                char type = Encoding.UTF8.GetChars(data, 1, 1)[0];
                 if (type.Equals('i'))
                 {
                     int a = BitConverter.ToInt32(data, 2);
                     int b = BitConverter.ToInt32(data, 6);
                     int sum = a + b;
-                    packet = new Packet(commandID,type, sum);
-                    Console.WriteLine(sum);
+                    packet = new Packet(commandID, type, sum);
+                    SendToClient(packet, sender);
+                }
+                else if (type.Equals('f'))
+                {
+                    float a = BitConverter.ToSingle(data, 2);
+                    float b = BitConverter.ToSingle(data, 6);
+                    float sum = a + b;
+                    packet = new Packet(commandID, type, sum);
+                    SendToClient(packet, sender);
+                }
+                else
+                {
+                    packet = new Packet(0, "Type must be both integer or float");
                     SendToClient(packet, sender);
                 }
 
@@ -54,17 +66,121 @@ namespace ServerMessagingApp
 
         public void Subtract(byte[] data, EndPoint sender)
         {
+            if (data != null && data.Length == 10)
+            {
+                byte commandID = data[0];
+                Packet packet = new Packet();
 
+                char type = Encoding.UTF8.GetChars(data, 1, 1)[0];
+                if (type.Equals('i'))
+                {
+                    int a = BitConverter.ToInt32(data, 2);
+                    int b = BitConverter.ToInt32(data, 6);
+                    int sub = a - b;
+                    packet = new Packet(commandID, type, sub);
+                    SendToClient(packet, sender);
+                }
+                else if (type.Equals('f'))
+                {
+                    float a = BitConverter.ToSingle(data, 2);
+                    float b = BitConverter.ToSingle(data, 6);
+                    float sub = a - b;
+                    packet = new Packet(commandID, type, sub);
+                    SendToClient(packet, sender);
+                }
+                else
+                {
+                    packet = new Packet(0, "Type must be both integer or float");
+                    SendToClient(packet, sender);
+                }
+
+            }
         }
 
         public void Multiply(byte[] data, EndPoint sender)
         {
+            if (data != null && data.Length == 10)
+            {
+                byte commandID = data[0];
+                Packet packet = new Packet();
 
+                char type = Encoding.UTF8.GetChars(data, 1, 1)[0];
+                if (type.Equals('i'))
+                {
+                    int a = BitConverter.ToInt32(data, 2);
+                    int b = BitConverter.ToInt32(data, 6);
+                    int mul = a * b;
+                    packet = new Packet(commandID, type, mul);
+                    SendToClient(packet, sender);
+                }
+                else if (type.Equals('f'))
+                {
+                    float a = BitConverter.ToSingle(data, 2);
+                    float b = BitConverter.ToSingle(data, 6);
+                    float mul = a * b;
+                    packet = new Packet(commandID, type, mul);
+                    SendToClient(packet, sender);
+                }
+                else
+                {
+                    packet = new Packet(0, "Type must be both integer or float");
+                    SendToClient(packet, sender);
+                }
+
+            }
         }
 
         public void Divide(byte[] data, EndPoint sender)
         {
+            if (data != null && data.Length == 10)
+            {
+                byte commandID = data[0];
+                Packet packet = new Packet();
 
+                char type = Encoding.UTF8.GetChars(data, 1, 1)[0];
+                if (type.Equals('i'))
+                {
+                    int a = BitConverter.ToInt32(data, 2);
+                    int b = BitConverter.ToInt32(data, 6);
+                    int division = 0;
+                    try
+                    {
+                    division = a / b;
+                    }
+                    catch (DivideByZeroException)
+                    {                       
+                        packet = new Packet(0, "The second number cannot be 0");
+                        SendToClient(packet, sender);
+                    }
+                    packet = new Packet(commandID, type, division);
+                    SendToClient(packet, sender);
+                }
+                else if (type.Equals('f'))
+                {
+                    float a = BitConverter.ToSingle(data, 2);
+                    float b = BitConverter.ToSingle(data, 6);
+                    float division = 0;
+                    try
+                    {
+                        division = a / b;
+                        if (float.IsNaN(division) || float.IsInfinity(division))
+                            throw new DivideByZeroException();
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        packet = new Packet(0, "The second number cannot be 0");
+                        SendToClient(packet, sender);
+                    }
+                    packet = new Packet(commandID, type, division);
+                    SendToClient(packet, sender);
+                }
+                else
+                {
+                    packet = new Packet(0, "Type must be both integer or float");
+                    SendToClient(packet, sender);
+                }
+
+            }
         }
 
         #endregion
